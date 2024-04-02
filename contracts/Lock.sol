9907c341c48c74accb3efa367ed9987820c52efd
Tschakki <tschakki@posteo.de>
2024-04-02 17:05:41 +0200

    Use solidity 0.8.4

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.4;
 
 import "@redstone-finance/evm-connector/contracts/data-services/RapidDemoConsumerBase.sol";
 
